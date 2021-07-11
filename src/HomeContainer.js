e247cad25e503522b49525fdc6eca1bbbb4dbc5d
iamibrahimkholil <Ibrahim.Kholil@teraark.com>
2021-07-11 09:23:47 +0600

    feat: done

--- src/HomeContainer.js
+++ src/HomeContainer.js
@@ -1,14 +1,51 @@
 import {Link} from 'react-router-dom';
 import moment from 'moment';
+import styled from 'styled-components';
+import Container from './components/helper/Container';
+import {Button} from './components/Button';
+
+const CenterEveryThing = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    min-height: 100vh;
+    width: 100%;
+`;
+
+const StyledLink = styled(Link)`
+    text-decoration: none;
+`;
+
+const FlexContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    
+    p{
+        width: 100%;
+        text-align: center;
+    }
+`;
+
+
 
 const HomeContainer = (props) => {
     const currentDate = moment().format('MM-YYYY').split('-');
     return(
-        <div>
-            Please visit /year/2020/month/1 
-            <br/>
-            <Link to={`/year/${currentDate[1]}/month/${currentDate[0]*1}`}>Or Click Start With Current Date</Link>
-        </div>
+        <Container>
+            <CenterEveryThing>
+                <FlexContainer>
+                    <p>Please visit /year/2020/month/1 </p>
+                    <br/>
+                    <StyledLink to={`/year/${currentDate[1]}/month/${currentDate[0]*1}`}>
+                        <Button>
+                            Or Click Start With Current Date
+                        </Button>
+                    </StyledLink>
+                </FlexContainer>
+            </CenterEveryThing>
+        </Container>
     )
 }
 
--- src/utills/Theme.js
+++ src/utills/Theme.js
@@ -15,16 +15,5 @@ export const defaultTheme = {
     textOnFormElementBackground: neutral[600],
     primaryFont,
     borderColor: neutral[700],
-    status: {
-        warningColor: yellow[100],
-        warningColorHover: yellow[200],
-        warningColorActive: yellow[300],
-        errorColor: red[100],
-        errorColorHover: red[200],
-        errorColorActive: red[300],
-        successColor: green[100],
-        successColorHover: green[200],
-        successColorActive: green[300]
-      }
 };
 
